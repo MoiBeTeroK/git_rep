f5d5dc97e4beea5d4332d681dd73f72c0fe5c9c0
MoiBeTeroK <127040139+MoiBeTeroK@users.noreply.github.com>
2024-11-16 21:18:34 +0300

    docs: update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 int main() {
-	// Переменная для имени пользователя
+	// A variable for the user name
     std::string name;
     std::cout << "Enter your name: ";
 	
-	// Ввод имени
+	// Entering a name
     std::cin >> name;
 	
-	// Вывод приветствия и имени
+	// Output of greeting and name
     std::cout << "Hello world from " << name << "!" << std::endl;
     return 0;
 }
