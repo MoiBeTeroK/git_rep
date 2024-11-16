e0ab3e1eed87f09035dcbcb4314879a0e297bf41
MoiBeTeroK <ludarikkot@gmail.com>
2024-11-16 20:13:22 +0300

    docs: add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 int main() {
+	// Переменная для имени пользователя
     std::string name;
     std::cout << "Enter your name: ";
+	
+	// Ввод имени
     std::cin >> name;
+	
+	// Вывод приветствия и имени
     std::cout << "Hello world from " << name << "!" << std::endl;
     return 0;
 }
